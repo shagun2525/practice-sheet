bcdb746085da1f1cb76d90cf59772f0d0454cff0
shagun2525 <140722426+shagun2525@users.noreply.github.com>
2023-08-11 12:40:29 +0530

    Add files via upload

---
+++ conditionals&booleans.py
@@ -0,0 +1,35 @@
+language = 'python'
+
+if language == 'python':
+   print('language is python')
+else:
+   print('no match')   
+
+language = 'java'
+
+if language == 'python':
+   print('language is python')
+else:
+   print('no match')
+
+language = 'java'
+
+if language == 'python':
+   print('language is python')
+elif language == 'java':
+    print('language is java')
+else:
+   print('no match')
+
+#and means both the keywords is true
+#or means one can be true than they can print the statement
+#not means opposite false is true and true is false
+
+
+
+
+
+
+
+
+
