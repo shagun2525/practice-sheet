f0715152cb1e1578a61654235ef824ce1a154d11
shagun2525 <140722426+shagun2525@users.noreply.github.com>
2023-08-11 12:54:33 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,26 @@
+#for even number
+'''
+a=int(input('enter a number'))
+if a%2==0:
+   print('number is even')
+else:
+   print('number is odd')
+'''
+
+# to find the simple interest 
+'''
+p=int(input('enter a number'))
+r=int(input('enter a number'))
+t=int(input('enter a number'))
+s=(p*r*t)/100
+print(s)
+'''
+
+#to find that person can vote above 18 or not vote less than 18 
+
+name=(input('enter name'))
+age=int(input('enter the age'))
+if age<18:
+    print('person cannot vote')
+else:
+    print('person can vote')
